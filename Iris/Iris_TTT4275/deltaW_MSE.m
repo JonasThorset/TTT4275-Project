28020d59a4914cde0e9a80008eb45965a82c56b2
JonasThorset <jonas.thorset@hotmail.no>
2020-03-26 14:25:33 +0100

    small changes

--- Iris/Iris_TTT4275/deltaW_MSE.m
+++ Iris/Iris_TTT4275/deltaW_MSE.m
@@ -3,7 +3,7 @@ function [outputArg1] = deltaW_MSE(X, T, W)
         for k = 1 : size(X, 2)
             g_k = 1./(1 + exp(-W*X(:,k)));       %Discriminant function
             t_k = T(:, k);                       %Class labels aka Targets
-            sum = sum + ((g_k -t_k).*g_k.*(1 - g_k))*X(:, k).';
+            sum = sum + ((g_k -t_k).*g_k.*(1 - g_k))*X(:, k)';
         end
         outputArg1 = sum;
 end
